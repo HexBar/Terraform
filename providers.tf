d23d1ac88219818dfed8b8083534743e316e8063
Bar <barmlaw01@gmail.com>
2023-08-02 21:24:04 +0300

    .

--- providers.tf
+++ providers.tf
@@ -1,20 +1,13 @@
+# Provider configuration
 terraform {
-  required_version = ">=0.12"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    tls = {
-      source = "hashicorp/tls"
-      version = "~>4.0"
+      version = "~>3.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
 }
